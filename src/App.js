f1334132012774d4d1f53f106edf83cd73a239f0
Andre Pires <brainshutdown@gmail.com>
2021-12-30 16:48:01 +0000

    Markdown react example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState, useRef } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
-  return <h2>markdown preview starter</h2>
+	const [markdown, setMarkdown] = useState('# Markdown');
+	const textAreaRef = useRef('');
+	return (
+		<main>
+			<section className='markdown'>
+				<textarea
+					ref={textAreaRef}
+					name='markdown'
+					id='markdown'
+					defaultValue={markdown}
+					cols='30'
+					rows='10'
+					onChange={() => setMarkdown(textAreaRef.current.value)}></textarea>
+				<article className='result'>
+					<ReactMarkdown>{markdown}</ReactMarkdown>
+				</article>
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
